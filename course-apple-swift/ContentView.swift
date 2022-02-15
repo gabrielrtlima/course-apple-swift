0f67a801b0087293458e324abca04c77262fa620
Gabriel Lima <gabriel.rtlima@upe.br>
2022-02-15 20:11:50 -0300

    Initial Commit

---
+++ course-apple-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  course-apple-swift
+//
+//  Created by Gabriel Rodrigues on 15/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ course-apple-swift/course_apple_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  course_apple_swiftApp.swift
+//  course-apple-swift
+//
+//  Created by Gabriel Rodrigues on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct course_apple_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
