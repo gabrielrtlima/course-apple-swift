41f5c077aa293726a1403d35e6c2c66a4824474b
Gabriel Lima <gabriel.rtlima@upe.br>
2022-02-15 21:17:53 -0300

    Criando CardView

---
+++ course-apple-swift/Models/DailyScrum.swift
@@ -0,0 +1,24 @@
+//
+//  DailyScrum.swift
+//  course-apple-swift
+//
+//  Created by Gabriel Rodrigues on 15/02/22.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
---
+++ course-apple-swift/Trailing.swift
@@ -0,0 +1,21 @@
+//
+//  Trailing.swift
+//  course-apple-swift
+//
+//  Created by Gabriel Rodrigues on 15/02/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
